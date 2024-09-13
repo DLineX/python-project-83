c5951cfacc8bc1286e165f1ef5299ff732bfe8c4
DaniilIslamgaliev <dante9226@mail.ru>
2024-09-13 14:20:32 +0300

    Update app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import os
 import psycopg2
+from flask import render_template
 
 from dotenv import load_dotenv
 
@@ -12,3 +13,7 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(DATABASE_URL)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
