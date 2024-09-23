2cac86d9d1b7341c9c9c74657e2af51e25df28d4
DaniilIslamgaliev <dante9226@mail.ru>
2024-09-23 16:45:31 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,17 @@
 DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS url_checks;
 
 CREATE TABLE urls (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) UNIQUE NOT NULL,
+    created_at DATE NOT NULL
+);
+CREATE TABLE url_checks (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id BIGINT,
+    status_code VARCHAR(255),
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description VARCHAR(255),
     created_at DATE NOT NULL
 );
