364f01889222a81f750391a4fe46895fa9da17f9
DaniilIslamgaliev <dante9226@mail.ru>
2024-09-23 17:52:59 +0300

    Update app.py
    Add normalize func, connect func, funcs for finding and checking urls.

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,20 +1,59 @@
 from flask import Flask
 import os
 import psycopg2
-from flask import render_template
+import requests
+from flask import render_template, flash, redirect, url_for
 
 from dotenv import load_dotenv
+from urllib.parse import urlparse
 
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-load_dotenv()
-DATABASE_URL = os.getenv('DATABASE_URL')
-conn = psycopg2.connect(DATABASE_URL)
+
+def connect():
+    database_url = os.getenv('DATABASE_URL')
+    conn = psycopg2.connect(database_url)
+    return conn
+
+
+def normalize(url):
+    o = urlparse(url)
+    return f'{o.scheme}://{o.netloc}'
+
+
+def find_url(id):
+    with connect().cursor() as cursor:
+        cursor.execute(
+            """
+            SELECT * FROM urls WHERE id=%(id)s;
+            """,
+            {"id": id}
+        )
+        url_id, name, created_at = cursor.fetchone()
+        return {
+            "id": url_id,
+            "name": name,
+            "created_at": created_at
+        }
 
 
 @app.route('/')
 def main():
     return render_template('index.html')
+
+
+# @app.route('/urls/<id>')
+# def url_show(id):
+
+
+@app.post('urls/<id>/checks')
+def url_check(id):
+    try:
+        requests.get(find_url(id)["name"]).raise_for_status()
+    except requests.exceptions.RequestException as ex:
+        print(ex)
+        flash("Неожиданная ошибка при проверке", "error")
+        return redirect(url_for("url_show", id=id))
