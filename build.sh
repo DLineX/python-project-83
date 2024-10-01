cd3b79e17da3698c9cd6d8b7f142d350902904bf
DaniilIslamgaliev <dante9226@mail.ru>
2024-10-01 17:38:13 +0300

    Attempt to fix

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #1/usr/bin/env bash
 
-make install && export PATH="/opt/render/project/poetry/bin:$PATH" && psql -a -d "$DATABASE_URL" -f database.sql
+make install && psql -a -d "$DATABASE_URL" -f database.sql
