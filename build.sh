9652e8f5556950a443bed690d2fc27d479c5bd3c
DaniilIslamgaliev <dante9226@mail.ru>
2024-10-01 17:28:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #1/usr/bin/env bash
 
-make install && psql -a -d $DATABASE_URL -f database.sql && export PATH="/opt/render/project/poetry/bin:$PATH"
+make install && export PATH="/opt/render/project/poetry/bin:$PATH" && psql -a -d "$DATABASE_URL" -f database.sql
