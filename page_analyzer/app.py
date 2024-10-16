609439c81a37b0aa8214c47df420163b3cba6332
DaniilIslamgaliev <dante9226@mail.ru>
2024-10-16 15:23:57 +0300

    Update code/database/template

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,26 +1,27 @@
 from flask import Flask
 import os
 import psycopg2
+from psycopg2.extras import NamedTupleCursor
 import requests
 from flask import (render_template, flash, request,
                    redirect, url_for, get_flashed_messages)
 
 from dotenv import load_dotenv
 from urllib.parse import urlparse
-from validators import url as validator
+import validators
 from datetime import date
 from bs4 import BeautifulSoup
 
 
 load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.secret_key = SECRET_KEY
 
 
 def connect():
     database_url = os.getenv('DATABASE_URL')
-    conn = psycopg2.connect(database_url)
-    return conn
+    return psycopg2.connect(database_url)
 
 
 def normalize(url):
@@ -38,7 +39,8 @@ def add_url(url):
             RETURNING id;""", {"name": url,
                                "created_at": created_at}
         )
-        return curs.fetchone()[0]
+        conn.commit()
+        return curs.fetchone()
 
 
 def find_url(id):
@@ -49,14 +51,7 @@ def find_url(id):
             SELECT * FROM urls WHERE id=%(id)s;
             """,
             {'id': id})
-        url_id = curs.fetchone()
-        name = curs.fetchone()
-        created_at = curs.fetchone()
-        return {
-            "id": url_id,
-            "name": name,
-            "created_at": created_at
-        }
+        return curs.fetchone()
 
 
 def exists_url(url):
@@ -68,144 +63,121 @@ def exists_url(url):
         )
         id = curs.fetchone()
         if id:
-            return id[0]
-        return False
+            return id
+        else:
+            return False
 
 
 def all_urls():
     conn = connect()
-    with conn.cursor() as curs:
+    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
         curs.execute(
             """SELECT
             urls.id,
             urls.name,
-            url_checks.created_at as created_at,
+            url_checks.created_at as date,
             url_checks.status_code
+            MAX(url_checks.url_id)
             FROM urls
             LEFT JOIN url_checks ON urls.id = url_checks.url_id
-            GROUP BY urls.id, urls.name, url_checks.status_code;""")
-        urls = []
-        rows = curs.fetchall()
-        for row in rows:
-            url = {
-                "id": row[0],
-                "name": row[1],
-                "created_at": row[2],
-                "status_code": row[3]
-            }
-            urls.append(url)
-        return urls
+            GROUP BY urls.id, urls.name, url_checks.status_code,
+            url_checks.created_at;""")
+        return curs.fetchall()
 
 
-@app.route('/')
+@app.get('/')
 def main():
     return render_template('form.html')
 
 
-@app.route('/urls')
+@app.get('/urls')
 def get_urls():
-    return render_template('urls.html', urls=all_urls())
+    urls = all_urls()
+    return render_template('urls.html', urls=urls)
 
 
 @app.post('/urls')
 def urls_add():
-    url = normalize(request.form.get('url'))
-    if not validator(url):
-        flash("Некорректный URL", "error")
+    url = request.form.to_dict()['url'].strip()
+    if not validators.url(url):
+        flash("Некорректный URL", "danger")
         return render_template(
-            'index.html',
-            messages=get_flashed_messages(with_categories=True)
+            'form.html',
+            url=url
         ), 422
-    if exists_url(url):
+    url = normalize(url)
+    url_finds = exists_url(url)
+    if url_finds:
         flash("Страница существует", "info")
-        return redirect(url_for('url_show', id=exists_url(url)))
-    flash("Страница успешно добавлена", "success")
-    return redirect(url_for('url_show', id=add_url(url)))
+        id = url_finds.id
+        return redirect(url_for('url_show', id=id))
+    else:
+        id = add_url(url)
+        flash("Страница успешно добавлена", "success")
+        return redirect(url_for('url_show', id=id.id))
 
 
 def all_checks(id):
     conn = connect()
-    with conn.cursor() as curs:
+    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
         curs.execute(
-            """SELECT id,
-            status_code,
-            COALESCE(h1,
-            title,
-            description),
-            DATE(created_at)
-            FROM url_checks
-            WHERE url_id = %s
-            ORDER BY id;""", (id,)
+            """SELECT * FROM url_checks
+            WHERE url_id = (%s);""", (id,)
         )
-        checks = []
-        rows = curs.fetchall()
-        for row in rows:
-            check = {
-                "id": row[0],
-                "status_code": row[1],
-                "h1": row[2],
-                "title": row[3],
-                "description": row[4],
-                "created_at": row[5]
-            }
-            checks.append(check)
-        return checks
+        return curs.fetchall()
 
 
-@app.route('/urls/<int:id>')
+@app.get('/urls/<int:id>')
 def url_show(id):
     url = find_url(id)
-    checks = all_checks(id)
+    url_check = all_checks(id)
     messages = get_flashed_messages(with_categories=True)
     return render_template(
         "show.html",
         url=url,
         messages=messages,
-        checks=checks
+        url_check=url_check
     )
 
 
-def check_url(id, status_code, h1, title, description):
-    created_at = str(date.today())
+def check_url(all_data):
     conn = connect()
-    with conn.cursor() as curs:
+    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
         curs.execute(
             """INSERT INTO url_checks (url_id, status_code, h1,
             title, description, created_at)
             VALUES (%(url_id)s, %(status_code)s, %(h1)s, %(title)s,
             %(description)s, %(created_at)s)
             RETURNING url_id, created_at;""",
-            {"url_id": id,
-             "status_code": status_code,
-             "h1": h1,
-             "title": title,
-             "description": description,
-             "created_at": created_at}
+            all_data
         )
+        conn.commit()
+
+
+def beautiful_soup(content):
+    soup = BeautifulSoup(content, 'lxml')
+    h1 = soup.h1.text if soup.h1 else ''
+    title = soup.title.text if soup.title else ''
+    description = ''
+    meta = soup.find("meta", {"name": "description"})
+    if meta:
+        description = meta.get('content', '')
+    return {'h1': h1,
+            'title': title,
+            'description': description}
 
 
 @app.post('/urls/<int:id>/checks')
 def url_check(id):
-    url = find_url(id)['name']
+    url = find_url(id)
     try:
-        response = requests.get(url)
+        response = requests.get(url.name)
         response.raise_for_status()
-        status_code = response.status_code
-        soup = BeautifulSoup(response.text, "lxml")
-        h1 = soup.find("h1")
-        h1 = h1.text if h1 else ""
-        title = soup.find("title")
-        title = title.text if title else ""
-        description = soup.find("meta", {"name": "description"})
-        description = description["content"] if description else ""
-        check_url(
-            id,
-            status_code=status_code,
-            h1=h1,
-            title=title,
-            description=description
-        )
+        all_data = beautiful_soup(response.text)
+        all_data['status_code'] = response.status_code
+        all_data['url_id'] = id
         flash("Url успешно проверен", "success")
+        check_url(all_data)
     except requests.exceptions.RequestException:
         flash("Произошла ошибка при проверке", "danger")
     finally:
